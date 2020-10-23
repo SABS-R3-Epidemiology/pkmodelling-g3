768e172cf57a241b0e9a8faf7b3d39d7c9db0bc6
Ioana B <ioana.bouros@dtc.ox.ac.uk>
2020-10-23 19:54:52 +0100

    add tests for visualisation

--- pkmodel/tests/test_visualisation.py
+++ pkmodel/tests/test_visualisation.py
@@ -1,7 +1,24 @@
-from unittest.mock import Mock  # noqa
-from pkmodel import Model, Protocol  # noqa
-import pkmodel.visualisation as pkv  # noqa
+"""
+Tests the :class:`Visualisation` module.
+"""
 
-# plot = Mock(name="myplting", return_value=)
-# pkv.plot_behaviour_one_experiment(Model(0, 1, [1], [0], 1), Protocol('intravenous', 'instantaneous', [5],  T=[10]), 10, 10)   # noqa
-# pkv.plot_comparison_experiments([Model(0, 1, [1], [0], 1), Model(0, 1, [1], [0], 1)], [Protocol('intravenous', 'instantaneous', [5],  T=[10]), Protocol('subcutaneous', 'continuous', 7, T=[10], absorption_rate=0.7)], 10, 10)  # noqa
+import unittest
+from unittest.mock import patch
+from pkmodel import Model, Protocol
+import pkmodel.visualisation as pkv
+
+
+class VisualisationTest(unittest.TestCase):
+    @patch("%s.pkv.plt" % __name__)
+    def test_one_plot(self, mock_plt):
+        pkv.plot_behaviour_one_experiment(Model(0, 1, [1], [0], 1), Protocol('intravenous', 'instantaneous', [5],  T=[10]), 10, 10)   # noqa
+
+        # Assert plt.figure is called once
+        mock_plt.figure.assert_called_once()
+
+    @patch("%s.pkv.plt" % __name__)
+    def test_two_plots(self, mock_plt):
+        pkv.plot_comparison_experiments([Model(0, 1, [1], [0], 1), Model(0, 1, [1], [0], 1)], [Protocol('intravenous', 'instantaneous', [5],  T=[10]), Protocol('subcutaneous', 'continuous', 7, T=[10], absorption_rate=0.7)], 10, 10)  # noqa
+
+        # Assert plt.figure is called once
+        mock_plt.figure.assert_called_once()
