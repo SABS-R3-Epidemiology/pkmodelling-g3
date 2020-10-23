d1d46fa43a2bfc9a2de4c541ecf8f001d71df2e2
Siting Miao <siting.miao@linacre.ox.ac.uk>
2020-10-23 13:23:33 +0100

    Update test for Solution Class

--- pkmodel/tests/test_solution.py
+++ pkmodel/tests/test_solution.py
@@ -1,5 +1,6 @@
 import unittest
 import pkmodel as pk
+import numpy as np
 
 
 class SolutionTest(unittest.TestCase):
@@ -10,6 +11,35 @@ class SolutionTest(unittest.TestCase):
         """
         Tests Solution creation.
         """
-        model = pk.Solution()
-        self.assertEqual(model.value, 44)
+        
+        with self.assertRaises(TypeError):
+            test_model = 0
+            test_protocol = pk.Protocol('subcutaneous','continuous',7,1000,T=[10])
+
+        with self.assertRaises(TypeError):
+            test_model = pk.Model(0, 1, [0], [0], 1)
+            test_protocol = 0
+
+    def test_solve(self):
+        """
+        Tests solve function
+        """
+         
+        # Test the situation when Dose(t) is constant at 0, then the solution will remain at 0
+        test_model = pk.Model(0, 1, [1], [0], 1)
+        test_protocol = pk.Protocol('intravenous', 'continuous', 0)
+        test_solution = solution(test_model, test_protocol, 10, 10)
+        test_sol = test_solution.solve
+
+        self.assertEqual(test_sol.y, np.zeros((3, 10)))
+     
+        # Test the situation when there is only the central compartment
+        test_model = pk.Model(1, 1, [], [], 2)
+        test_protocol = pk.Protocol('intravenous', 'continuous', 1)
+        test_solution = Solution(test_model, test_protocol, 10, 10)
+        test_sol = test_solution.solve
+
+        self.assertEqual(test_sol.y, np.vstack((1 - np.exp(-test_sol.t), np.zeros((1, 10))))
+
+        
 
