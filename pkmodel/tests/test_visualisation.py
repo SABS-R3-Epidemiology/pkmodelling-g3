7a0c9b8ec1edb21c2760d3038470872dceb69506
Ioana B <ioana.bouros@dtc.ox.ac.uk>
2020-10-23 18:43:33 +0100

    update test_visualisation.py

---
+++ pkmodel/tests/test_visualisation.py
@@ -0,0 +1,7 @@
+from unittest.mock import Mock  # noqa
+from pkmodel import Model, Protocol  # noqa
+import pkmodel.visualisation as pkv  # noqa
+
+# plot = Mock(name="myplting", return_value=)
+# pkv.plot_behaviour_one_experiment(Model(0, 1, [1], [0], 1), Protocol('intravenous', 'instantaneous', [5],  T=[10]), 10, 10)   # noqa
+# pkv.plot_comparison_experiments([Model(0, 1, [1], [0], 1), Model(0, 1, [1], [0], 1)], [Protocol('intravenous', 'instantaneous', [5],  T=[10]), Protocol('subcutaneous', 'continuous', 7, T=[10], absorption_rate=0.7)], 10, 10)  # noqa
