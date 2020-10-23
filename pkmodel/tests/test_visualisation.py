bb65bf905367fc467faff0f2ff25b6ccb4776196
I-Bouros <72917474+I-Bouros@users.noreply.github.com>
2020-10-23 19:14:59 +0100

    Do this later

--- pkmodel/tests/test_visualisation.py
+++ pkmodel/tests/test_visualisation.py
@@ -1,6 +1,6 @@
-from unittest.mock import Mock  # noqa
-from pkmodel import Model, Protocol  # noqa
-import pkmodel.visualisation as pkv  # noqa
+# from unittest.mock import Mock  # noqa
+# from pkmodel import Model, Protocol  # noqa
+# import pkmodel.visualisation as pkv  # noqa
 
 # plot = Mock(name="myplting", return_value=)
 # pkv.plot_behaviour_one_experiment(Model(0, 1, [1], [0], 1), Protocol('intravenous', 'instantaneous', [5],  T=[10]), 10, 10)   # noqa
